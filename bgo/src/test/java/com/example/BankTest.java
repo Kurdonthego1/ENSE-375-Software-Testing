4e66254dc6dcbce241695b9eeefbf86814dff189
Aaron Borja <aaronborja219@gmail.com>
2025-07-22 19:35:54 -0600

    Changed deposit tests

--- bgo/src/test/java/com/example/BankTest.java
+++ bgo/src/test/java/com/example/BankTest.java
@@ -104,5 +104,76 @@ public class BankTest {
 
 
 
-    
+
+    @Test
+    void testAddAccountSavings(){
+        Bank bank = new Bank();
+        bank.deleteAccount("testLogin", "savings");
+        boolean result = bank.addAccount("testLogin","savings");
+        assertTrue(result);
+    }
+
+    @Test
+    void testAddAccountChequing(){
+        Bank bank = new Bank();
+        bank.deleteAccount("testLogin", "chequing");
+        boolean result = bank.addAccount("testLogin","chequing");
+        assertTrue(result);
+    }
+
+    @Test
+    void testAddAccountChequingAndSavings(){
+        Bank bank = new Bank();
+        bank.deleteAccount("testLogin", "chequing");
+        bank.deleteAccount("testLogin", "savings");
+        boolean result = bank.addAccount("testLogin","chequing");
+        boolean result2 = bank.addAccount("testLogin","savings");
+        assertTrue(result && result2);
+    }
+
+    @Test
+    void testAddAccountChequingTwice_Fail(){
+        Bank bank = new Bank();
+        bank.deleteAccount("testLogin", "chequing");
+        bank.addAccount("testLogin","chequing");
+        boolean result = bank.addAccount("testLogin","chequing");
+        assertFalse(result);
+    }
+
+    @Test
+    void testAccountThatsNotAnOption_Fail(){
+        Bank bank = new Bank();
+        boolean result = bank.addAccount("testLogin","Roblox");
+        assertFalse(result);
+    }
+
+    @Test
+    void testDeleteAccountSavings(){
+        Bank bank = new Bank();
+        bank.deleteAccount("testLogin", "savings");
+        bank.addAccount("testLogin","savings");
+        boolean result = bank.deleteAccount("testLogin","savings");
+        assertTrue(result);
+    }
+
+    void testDeleteAccountChequing(){
+        Bank bank = new Bank();
+        bank.deleteAccount("testLogin", "chequing");
+        bank.addAccount("testLogin","chequing");
+        boolean result = bank.deleteAccount("testLogin","chequing");
+        assertTrue(result);
+    }
+
+    void testDeleteNonExistingAccount_Fail(){
+        Bank bank = new Bank();
+        bank.deleteAccount("testLogin", "chequing");
+        boolean result = bank.deleteAccount("testLogin","chequing");
+        assertFalse(result);
+    }
+
+    void testDeleteNonAvailableAccount_Fail(){
+        Bank bank = new Bank();
+        boolean result = bank.deleteAccount("testLogin","roblox");
+        assertFalse(result);
+    }
 }
